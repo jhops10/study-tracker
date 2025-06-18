8842174086fc3dce16518f9b646199348d190d08
João Paulo <joaowais@gmail.com>
2025-06-18 20:21:16 -0300

    first commit

---
+++ src/main/java/com/jhops10/study_tracker/StudyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jhops10.study_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhops10/study_tracker/StudyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhops10.study_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
