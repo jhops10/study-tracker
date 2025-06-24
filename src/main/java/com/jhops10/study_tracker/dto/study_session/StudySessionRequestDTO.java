3128a54c8db0e43b3c7e25c9162040a42dc51b73
João Paulo <joaowais@gmail.com>
2025-06-23 22:40:52 -0300

    feat: Ajusta formatação de datas para o padrão dd/MM/yyyy nos DTOs

--- src/main/java/com/jhops10/study_tracker/dto/study_session/StudySessionRequestDTO.java
+++ src/main/java/com/jhops10/study_tracker/dto/study_session/StudySessionRequestDTO.java
@@ -1,5 +1,6 @@
 package com.jhops10.study_tracker.dto.study_session;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
@@ -10,6 +11,7 @@ import java.util.Date;
 public record StudySessionRequestDTO(
         @NotBlank(message = "A data da sessão é obrigatória")
         @PastOrPresent(message = "A data não pode estar no futuro")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
         Date date,
         @NotBlank(message = "O tópico é obrigatório")
         String topic,
