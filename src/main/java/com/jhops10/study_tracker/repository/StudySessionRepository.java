e6e699ee1c08c19cf2bf428b440104874fe89de7
João Paulo <joaowais@gmail.com>
2025-06-23 22:15:46 -0300

    feat: Adiciona StudySessionRepository

---
+++ src/main/java/com/jhops10/study_tracker/repository/StudySessionRepository.java
@@ -0,0 +1,9 @@
+package com.jhops10.study_tracker.repository;
+
+import com.jhops10.study_tracker.model.StudySession;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudySessionRepository extends JpaRepository<StudySession, Long> {
+}
